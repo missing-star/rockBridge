c6e79450101600f7ada11643ba80a106ff8edefc
missing-star <1652575963@qq.com>
2019-02-20 14:29:02 +0800

    首页可点击

--- js/business-recommend.js
+++ js/business-recommend.js
@@ -1,7 +1,7 @@
 var vm = new Vue({
     el:'#app',
     data:{
-
+        shopsList:[]
     },
     methods:{
         enterShop(id) {
@@ -9,6 +9,37 @@ var vm = new Vue({
             mui.openWindow({
                 url: `shop-detail.html?id=${id}`
             })
+        },
+        getDetail(url,id) {
+            mui.openWindow({
+                url:`${url}?id=${id}`
+            })
+        }
+    },
+    filters:{
+        filterImg(thumb) {
+            thumb = thumb == null ? '' : thumb;
+            if(thumb.indexOf('http') != -1) {
+                return `${thumb}`;
+            }
+            return `${rootUrl}${thumb}`;
         }
     }
-})
+});
+
+getShopsList();
+function getShopsList() {
+    $.ajax({
+        url:`${rootUrl}/index/api/getHotShops`,
+        dataType:'json',
+        type:'post',
+        success:function(data) {
+            if(data.status == 1) {
+                vm.shopsList = data.result;
+            }
+        },
+        error:function() {
+            mui.toast('服务器异常');
+        }
+    });
+}
