215b1b55ffc7a5923a48b1ec8ad1cf5604a9ea00
missing-star <1652575963@qq.com>
2019-02-20 14:06:51 +0800

    图片显示

--- js/shop-data-setting.js
+++ js/shop-data-setting.js
@@ -217,5 +217,5 @@ function dataURLtoFile(dataurl, filename) {
     //转换成file对象
     return new File([u8arr], filename, {type:mime});
     //转换成成blob对象
-    return new Blob([u8arr],{type:mime});
+    // return new Blob([u8arr],{type:mime});
   }
