77be90e3aee096fce9662307eed68808d6931fa6
missing-star <1652575963@qq.com>
2019-02-20 13:59:47 +0800

    有好物接口

--- js/have-good-goods.js
+++ js/have-good-goods.js
@@ -1,9 +1,42 @@
-var vm = new Vue({
+const vm = new Vue({
     el:'#app',
     data:{
-
+        adList:[]
     },
     methods:{
-
+        goLink(url) {
+            mui.openWindow({
+                url:url
+            })
+        }
+    },
+    filters:{
+        filterImg(thumb) {
+            thumb = thumb == null ? '' : thumb;
+            if(thumb.indexOf('http') != -1) {
+                return `${thumb}`;
+            }
+            return `${rootUrl}${thumb}`;
+        }
     }
-})
+});
+
+getAdList();
+function getAdList() {
+    $.ajax({
+        url:`${rootUrl}/index/api/getPopularityGoods`,
+        dataType:'json',
+        type:'post',
+        data:{
+            type:2
+        },
+        success:function(data) {
+            if(data.status == 1) {
+                vm.adList = data.result;
+            }
+        },
+        error:function() {
+            mui.toast('服务器异常');
+        }
+    });
+}
