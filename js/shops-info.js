cb01e4dfd19b46b9f8592a7f8a7ab268ae6d35a8
missing-star <1652575963@qq.com>
2019-03-04 14:55:52 +0800

    地址

--- js/shops-info.js
+++ js/shops-info.js
@@ -5,7 +5,8 @@ var vm = new Vue({
             shop_code:'',
             shop_name:'',
             person_name:'',
-            phone:''
+            phone:'',
+            shop_address:''
         }
     },
     methods: {
@@ -30,6 +31,7 @@ function getShopsInfo(shops_id) {
                 vm.shop_name = data.result.title;
                 vm.person_name = data.result.person_name;
                 vm.phone = data.result.phone;
+                vm.shop_address = data.result.province + data.result.city + data.result.address;
             }
             else if(data.status == 202) {
                 goLogin();
